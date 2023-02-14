da66ce66265bbb52736e6509ba135535c552a6e7
gorkemu <gorkemunal88@gmail.com>
2023-02-14 05:33:49 +0300

    add products

---
+++ src/data.js
@@ -0,0 +1,48 @@
+const data = {
+  products: [
+    {
+      id: "1",
+      name: "Eventide H9",
+      price: 744,
+      image:
+        "https://images.unsplash.com/photo-1528099457806-e19e7ae792e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    },
+    {
+      id: "2",
+      name: "Ibanez Tube Screamer",
+      price: 159,
+      image:
+        "https://images.unsplash.com/photo-1612306178575-6611cac27c51?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    },
+    {
+      id: "3",
+      name: "Wampler Low Blow",
+      price: 209,
+      image:
+        "https://images.unsplash.com/photo-1525782163864-a4903789894c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80",
+    },
+    {
+      id: "4",
+      name: "Marshall Plexi",
+      price: 799,
+      image:
+        "https://images.unsplash.com/photo-1510104183447-b598400bb62c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    },
+    {
+      id: "5",
+      name: "Fender Vibroverb Amp",
+      price: 1099,
+      image:
+        "https://images.unsplash.com/photo-1565829073670-cd08d8c63643?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    },
+    {
+      id: "6",
+      name: "Bugera",
+      price: 999,
+      image:
+        "https://images.unsplash.com/photo-1463134443644-45f4dc57a612?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80",
+    },
+  ],
+};
+
+export default data;
