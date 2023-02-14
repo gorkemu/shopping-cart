5fe7cea62294d7f9abb49255efa4921d92f1350c
gorkemu <gorkemunal88@gmail.com>
2023-02-14 05:36:39 +0300

    create Shop page

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Product from "./Product";
 
 const Shop = (props) => {
-  return <div className="Shop"></div>;
+  const { products, onAdd } = props;
+
+  return (
+    <div className="Shop">
+      <div className="products">
+        <h2>Amplifiers & Effects</h2>
+        <hr />
+        <div className="products-list">
+          {products.map((product) => (
+            <Product onAdd={onAdd} key={product.id} product={product}></Product>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
