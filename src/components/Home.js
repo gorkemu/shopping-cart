a6fbe9d373f75cd19dc73fd82bf2dddc31fd11c0
gorkemu <gorkemunal88@gmail.com>
2023-02-15 00:00:49 +0300

    add Home & create Link to Shop

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = (props) => {
-    return (
-        <div>
-            Home
-        </div>
-    )
-}   
+  return (
+    <div
+      className="Home"
+      style={{
+        backgroundImage: `url:("https://images.unsplash.com/photo-1546367791-e7447b431084?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80")`,
+        height: "100vh",
+      }}
+    >
+      <div className="home-text">
+        <h2 className="home-title">PUSH YOUR TONE</h2>
+        <p>TONES YOU'LL WANT TO KEEP GOING FOREVER</p>
+        <Link to="/shop">
+          <button className="btn shop-now-btn">Shop Now</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
