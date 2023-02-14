c4700902c90cfe8a0325b4433f1b2d1e341d8b97
gorkemu <gorkemunal88@gmail.com>
2023-02-15 00:40:17 +0300

    render Add to Cart and +/- buttons conditionally

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,10 @@
 import React from "react";
 
 const Product = (props) => {
-  const { product, onAdd } = props;
+  const { product, cartItems, onAdd, onRemove } = props;
+
+  const isInTheCart = cartItems.find((item) => item.id === product.id);
+
   return (
     <div className="Product">
       <img
@@ -11,9 +14,27 @@ const Product = (props) => {
       ></img>
       <div className="product-name">{product.name}</div>
       <div className="product-price">${product.price.toLocaleString()}</div>
-      <button onClick={() => onAdd(product)} className="btn add-to-cart-btn">
-        Add to Cart
-      </button>
+      {isInTheCart ? (
+        <div className="add-remove-btns">
+          <button
+            className="btn add-remove-btn qty-btn"
+            onClick={() => onRemove(product)}
+          >
+            -
+          </button>
+          <span>{isInTheCart.qty}</span>
+          <button
+            className="btn add-remove-btn qty-btn"
+            onClick={() => onAdd(product)}
+          >
+            +
+          </button>
+        </div>
+      ) : (
+        <button onClick={() => onAdd(product)} className="btn add-to-cart-btn">
+          Add to Cart
+        </button>
+      )}
     </div>
   );
 };
